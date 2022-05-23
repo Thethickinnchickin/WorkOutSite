b4e9400bc46fc968c20fdf9acac9eb2b650c6aec
Thethickinnchickin <matt.reiley@sjsu.edu>
2022-05-22 18:54:15 -0700

    Initial commit:

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const dbUrl = "mongodb+srv://workout-user:OaklandA's54@cluster0.vnc1b.mongodb.net/?retryWrites=true&w=majority";
+
+//Creating mongo store with session
+const store = new MongoStore ({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'super super secret'
+    }
+})
+
+app.use(session({
+    store,
+    secret: 'super super secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}));
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+//Adding passport to express and authenticating user
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//starting up app
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
